d0de5ddf2a189669e4f4432d0fff0c8f8b752a0a
AydanMammadlii <aydanmma@code.edu.az>
2024-05-14 22:46:00 +0400

    Add ProductType DTOs

---
+++ Corano-main/src/CoronaApi/Infrastructure/Corona.Persistance/Implementations/Services/ProductTypeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corona.Persistance.Implementations.Services
+{
+    internal class ProductTypeService
+    {
+    }
+}
