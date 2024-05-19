13c6328127cd9c256841185868e98f52e9086991
AydanMammadlii <aydanmma@code.edu.az>
2024-05-19 23:32:36 +0400

    Add BasketProduct Profile

---
+++ Corano-main/src/CoronaApi/Infrastructure/Corona.Persistance/MapperProfiles/BasketProductProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Corona.Application.DTOs.BasketProducts;
+using Corona.Domain.Entities;
+
+namespace Corona.Persistance.MapperProfiles;
+
+public class BasketProductProfile : Profile
+{
+    public BasketProductProfile()
+    {
+        CreateMap<BasketProduct, CreateBasketProductDto>().ReverseMap();
+        CreateMap<BasketProduct, UpdateBasketProductDto>().ReverseMap();
+        CreateMap<BasketProduct, GetBasketProductDto>().ReverseMap();
+    }
+}
