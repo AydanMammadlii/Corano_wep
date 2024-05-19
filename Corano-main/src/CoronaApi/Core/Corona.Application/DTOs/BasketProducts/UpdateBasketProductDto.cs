f80b845e7ddb37d89212a6a4bda54a6724b7ca4e
AydanMammadlii <aydanmma@code.edu.az>
2024-05-19 21:55:33 +0400

    Add BasketProduct DTOs

---
+++ Corano-main/src/CoronaApi/Core/Corona.Application/DTOs/BasketProducts/UpdateBasketProductDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Corona.Application.DTOs.BasketProducts;
+
+public class UpdateBasketProductDto
+{
+    public Guid ProductId { get; set; }
+    public Guid BasketId { get; set; }
+    public int Count { get; set; } = 0;
+}
