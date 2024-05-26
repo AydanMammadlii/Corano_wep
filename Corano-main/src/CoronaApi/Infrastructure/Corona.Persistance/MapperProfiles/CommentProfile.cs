cc13272669bc99d90c5f742c093944241523556e
AydanMammadlii <aydanmma@code.edu.az>
2024-05-26 21:45:14 +0400

    Add Comment Profile

---
+++ Corano-main/src/CoronaApi/Infrastructure/Corona.Persistance/MapperProfiles/CommentProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Corona.Application.DTOs.Auth;
+using Corona.Application.DTOs.Comments;
+using Corona.Domain.Entities;
+
+namespace Corona.Persistance.MapperProfiles;
+
+public class CommentProfile : Profile
+{
+    public CommentProfile()
+    {
+        CreateMap<Comment, CreateCommentDto>().ReverseMap();
+        CreateMap<Comment, GetCommentDto>().ForMember(dest => dest.AppUserDto, opt => opt.MapFrom(src => src.AppUser))
+            .ReverseMap();
+        CreateMap<AppUser, AppUserDto>().ReverseMap();
+    }
+}
