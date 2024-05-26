23e4c12c581b92605c3749802c89d5bc177ad009
AydanMammadlii <aydanmma@code.edu.az>
2024-05-26 14:36:09 +0400

    Add Comment DTOs

---
+++ Corano-main/src/CoronaApi/Core/Corona.Application/DTOs/Comments/CreateCommentDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Corona.Application.DTOs.Comments;
+
+public class CreateCommentDto
+{
+    public string CommentText { get; set; }
+    public Guid BlogId { get; set; }
+    public string AppUserId { get; set; }
+}
