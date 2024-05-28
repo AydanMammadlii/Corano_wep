7df2efe62500b87496bc6f0b4d9a12b4d248263f
AydanMammadlii <aydanmma@code.edu.az>
2024-05-28 22:12:16 +0400

    Add CategorysController

---
+++ Corano-main/src/CoronaApi/Presentation/Corona.API/Controllers/CategorysController.cs
@@ -0,0 +1,55 @@
+﻿using Corona.Application.Abstraction.Services;
+using Corona.Application.DTOs.Brands;
+using Corona.Application.DTOs.Categorys;
+using Corona.Persistance.Implementations.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Corona.API.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class CategorysController : ControllerBase
+{
+    private readonly ICategoryService _categoryService;
+
+    public CategorysController(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var category = await _categoryService.GetAllAsync();
+        return Ok(category);
+    }
+
+    [HttpGet("{id:Guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var category = await _categoryService.GetByIdAsync(id);
+        return Ok(category);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateCategory([FromForm] CreateCategoryDto createCategoryDto)
+    {
+        await _categoryService.CreateAsync(createCategoryDto);
+        return StatusCode((int)HttpStatusCode.Created);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateCategory([FromForm] UpdateCategoryDto updateCategoryDto)
+    {
+        await _categoryService.UpdateAsync(updateCategoryDto);
+        return Ok();
+    }
+
+    [HttpDelete("{id:Guid}")]
+    public async Task<IActionResult> Remove(Guid id)
+    {
+        await _categoryService.RemoveAsync(id);
+        return Ok();
+    }
+}
--- Corano-main/src/CoronaApi/Presentation/Corona.API/obj/Debug/net8.0/Corona.API.AssemblyInfo.cs
+++ Corano-main/src/CoronaApi/Presentation/Corona.API/obj/Debug/net8.0/Corona.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Corona.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc0fbb23abcc85bac6faacc36b5310b8914673b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0400fddb078d049c9289b523eed589963c08f0b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Corona.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Corona.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
