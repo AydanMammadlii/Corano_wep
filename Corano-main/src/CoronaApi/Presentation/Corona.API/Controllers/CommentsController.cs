9ec20bcade828e92a46cd2b689cc12e8d940d802
AydanMammadlii <aydanmma@code.edu.az>
2024-05-28 23:37:23 +0400

    Add CommentsController

---
+++ Corano-main/src/CoronaApi/Presentation/Corona.API/Controllers/CommentsController.cs
@@ -0,0 +1,42 @@
+﻿using Corona.Application.Abstraction.Services;
+using Corona.Application.DTOs.Brands;
+using Corona.Application.DTOs.Comments;
+using Corona.Persistance.Implementations.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Corona.API.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class CommentsController : ControllerBase
+{
+    private readonly ICommentService _commentService;
+
+    public CommentsController(ICommentService commentService)
+    {
+        _commentService = commentService;
+    }
+
+
+    [HttpGet("{BlogId:Guid}")]
+    public async Task<IActionResult> GetAll(Guid BlogId)
+    {
+        var comment = await _commentService.GetAllAsync(BlogId);
+        return Ok(comment);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateComment([FromForm] CreateCommentDto createCommentDto)
+    {
+        await _commentService.CreateAsync(createCommentDto);
+        return StatusCode((int)HttpStatusCode.Created);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Remove([FromForm] RemoveCommentDto removeCommentDto)
+    {
+        await _commentService.RemoveAsync(removeCommentDto);
+        return Ok();
+    }
+}
--- Corano-main/src/CoronaApi/Presentation/Corona.API/obj/Debug/net8.0/Corona.API.AssemblyInfo.cs
+++ Corano-main/src/CoronaApi/Presentation/Corona.API/obj/Debug/net8.0/Corona.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Corona.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0400fddb078d049c9289b523eed589963c08f0b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7df2efe62500b87496bc6f0b4d9a12b4d248263f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Corona.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Corona.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
