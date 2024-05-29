ed8d0e0ef51dcdf2a295a1c7487e635bb63b9037
AydanMammadlii <aydanmma@code.edu.az>
2024-05-29 11:41:12 +0400

    Add Controllers and fix

---
+++ Corano-main/src/CoronaApi/Presentation/Corona.API/Controllers/WishlistProductsController.cs
@@ -0,0 +1,40 @@
+﻿using Corona.Application.Abstraction.Services;
+using Corona.Application.DTOs.BasketProducts;
+using Corona.Application.DTOs.WishlistDto;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Corona.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class WishlistProductsController : ControllerBase
+{
+    private readonly IWishlistProductService _wishlistProductService;
+    public WishlistProductsController(IWishlistProductService wishlistProductService)
+    {
+        _wishlistProductService = wishlistProductService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll(string AppUserId)
+    {
+        var wishlistProduct = await _wishlistProductService.GetAllAsync(AppUserId);
+        return Ok(wishlistProduct);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateBasketProduct([FromForm] CreateWishlistProductDto createWishlistProductDto)
+    {
+        await _wishlistProductService.CreateAsync(createWishlistProductDto);
+        return StatusCode((int)HttpStatusCode.Created);
+    }
+
+
+    [HttpDelete]
+    public async Task<IActionResult> Remove([FromForm] RemoveWishlistProductDto removeWishlistProductDto)
+    {
+        await _wishlistProductService.RemoveAsync(removeWishlistProductDto);
+        return Ok();
+    }
+}
